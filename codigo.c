81d173784a942df2a95a4b275cd2d8afd5ce2881
EmiSancho <emilysancho@gmail.com>
2020-06-06 11:54:15 -0600

    estructura basica del codigo

---
+++ codigo.c
@@ -0,0 +1,26 @@
+/**
+ * Made by Emilly Sancho Murillo and Daniel Bogarin * 
+ * for TI4202 Algoritmos y estructuras de datos.
+ * Teacher: Maria Jose Artavia.
+ * IS 2020 - Homework 3 - 06/06/2020
+ */
+
+#include <stdio.h>
+
+
+void RadixSort(int lista[]){
+
+}
+
+int findLinealSearch(int element){
+
+}
+
+int findBinarySearch(int element){
+
+}
+
+
+int main() {
+    return 0;
+}
